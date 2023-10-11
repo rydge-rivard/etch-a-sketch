202e571f630adc0ac8a515689527143e50bc97e0
Rydge Rivard <96926831+rydge-rivard@users.noreply.github.com>
2023-10-11 09:20:02 +0100

    Create DOM grid with js and css.

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-document.querySelector('.grid');
+function createGrid () {
+    const grid = document.querySelector('.grid');
+    for (let i = 0; i < 256; i++) {
+        const div = document.createElement('div');
+        //div.setAttribute('style', 'border-style: solid; border-color: black;');
+        grid.appendChild(div);
+    }
+}
 
+createGrid();
