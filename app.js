adea97c52d9c11c7409006f06f9a9d9609e1f058
Rydge Rivard <96926831+rydge-rivard@users.noreply.github.com>
2023-10-11 09:36:07 +0100

    Add class to DOM grids for colouring later.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function createGrid () {
     const grid = document.querySelector('.grid');
     for (let i = 0; i < 256; i++) {
         const div = document.createElement('div');
-        //div.setAttribute('style', 'border-style: solid; border-color: black;');
+        div.classList.add('grid-square');
         grid.appendChild(div);
     }
 }
