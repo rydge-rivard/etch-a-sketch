cb24c4d45010087b8e0124b13d93a1fdf57118e0
Rydge Rivard <96926831+rydge-rivard@users.noreply.github.com>
2023-10-11 12:58:25 +0100

    Dynamically set grid length with button via event listener.

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
-function createGrid () {
+createBaseGrid(16);
+
+function createBaseGrid (gridLength) {
     const grid = document.querySelector('.grid');
-    for (let i = 0; i < 256; i++) {
+    numberOfGridSquares = gridLength ** 2;
+    for (let i = 0; i < numberOfGridSquares; i++) {
         const div = document.createElement('div');
+        //div.setAttribute('style', `height: ${700/gridLength}px; width: ${700/gridLength}px;`);
+        div.style.height = 700/gridLength + "px";
+        div.style.width = 700/gridLength + "px";
         div.classList.add('grid-square');
+        div.addEventListener('mouseover', () => {
+            div.style.background = "blue";
+        });
         grid.appendChild(div);
     }
 }
 
-createGrid();
+let gridSquare = document.querySelectorAll('.grid-square');
+const btn = document.querySelector('.select-grid');
+btn.addEventListener('click', createNewGrid);
 
-const gridSquare = document.querySelectorAll('.grid-square');
-
-gridSquare.forEach((square) => {
+/*gridSquare.forEach((square) => {
     square.addEventListener('mouseover', () => {
-        square.setAttribute('style', 'background: blue;');
+        square.setAttribute('style', `background: blue;`)
     });
 });
+*/
+
+function createNewGrid(gridLength) {
+    gridLength = prompt('How many squares per side should the grid be?');
+    if (gridLength > 100) {
+        alert('Please select a value less than 100.')
+        createNewGrid(gridLength);
+    } else {
+        console.log(gridLength);
+        removeOldGrid();
+        createBaseGrid(gridLength);
+    }
+}
+
+function removeOldGrid() {
+    gridSquare = document.querySelectorAll('.grid-square');
+    gridSquare.forEach((square) => {
+        square.remove();
+    })
+}
